8f40a9a3548f5f13f2a0d8b34c1ef83a06489547
Fenrier719 <vlad21072001@gmail.com>
2021-07-28 15:45:02 +0300

    changed profileserializer

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserSerializer
+from drf_writable_nested import WritableNestedModelSerializer
 from users.models import Profile
 
 User = get_user_model()
 
 
-class ProfileSerializer(serializers.ModelSerializer):
+class ProfileSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
     user = UserSerializer()
 
     class Meta:
