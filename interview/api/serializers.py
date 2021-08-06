5c966ea05f911cf15e7dcc544796aea9271b96cc
Fenrier719 <vlad21072001@gmail.com>
2021-08-06 13:29:52 +0300

    refactored interview serializer

--- interview/api/serializers.py
+++ interview/api/serializers.py
@@ -14,6 +14,7 @@ class InterviewCreateSerializer(WritableNestedModelSerializer, serializers.Model
             'interviewer',
             'interviewed',
             'date_of_interview',
+            'is_active',
         )
 
 
